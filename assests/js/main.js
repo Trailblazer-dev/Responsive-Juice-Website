7007e1211f6bec25042beee63f702aa56f365752
Trailblazer-dev <richvictor830@gmail.com>
2024-07-28 17:35:32 +0300

    final touches

--- assests/js/main.js
+++ assests/js/main.js
@@ -32,7 +32,7 @@ const swiperMenu = new Swiper('.menu__container', {
   grabCursor: true,
   direction:'horizontal',
   spaceBetween:24,
-  centeredSlides: true,
+  freeMode:'true',
   loop:true,
 
   // If we need pagination
@@ -46,7 +46,7 @@ const swiperMenu = new Swiper('.menu__container', {
     },
     768: {
       slidesPerView: 3,
-      spaceBetween: 40,
+      spaceBetween: 30,
     },
   }
   // Navigation arrows
@@ -90,20 +90,20 @@ contactLinks.forEach((link, index) => {
 
 // REVEAL 
 
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "60px",
-//   duration: 2500,
-//   delay:400,
-//   reset: true,
-// })
-// sr.reveal('.home__containerr')
-// sr.reveal('.features__images', {origin: 'bottom',delay:600})
-// sr.reveal('.features__data', {delay:600,origin:'left'})
-// sr.reveal('.menu__container', {delay:800,origin:'left'})
-// sr.reveal('.menu__title',{origin:'bottom',delay:700})
-// sr.reveal('.contact__container',{delay:1000})
-// sr.reveal('.show',{delay:500,origin:'bottom'})
-// // sr.reveal('.aboutus__data',{delay:900,origin:'right'})
-// // sr.reveal('.aboutus__image,.services__image,.blog__image',{delay:900,origin:'left'})
-// sr.reveal('.footer',{delay:1200,origin:'bottom'})
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay:400,
+  reset: true,
+})
+sr.reveal('.home__containerr')
+sr.reveal('.features__images', {origin: 'bottom',delay:600})
+sr.reveal('.features__data', {delay:600,origin:'left'})
+sr.reveal('.menu__container', {delay:800,origin:'left'})
+sr.reveal('.menu__title',{origin:'bottom',delay:700})
+sr.reveal('.contact__container',{delay:1000})
+sr.reveal('.show',{delay:500,origin:'bottom'})
+// sr.reveal('.aboutus__data',{delay:900,origin:'right'})
+// sr.reveal('.aboutus__image,.services__image,.blog__image',{delay:900,origin:'left'})
+sr.reveal('.footer',{delay:1200,origin:'bottom',distance:'10px'})
